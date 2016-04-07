92b8c2cdaba5dc98887ca57e8df0a427b0a177cb
L <govindaraj.l@honeywell.com>
2016-04-07 16:32:18 +0530

    commit

---
+++ NewMVCApp/NewMVCApp/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewMVCApp.ViewModels;
+using NewMVCApp.Models;
+
+namespace NewMVCApp.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(AccountViewModel avm)
+        {
+           
+                if (avm.Username.Equals("abc") && avm.Password.Equals("1234"))
+                {
+                    //Session["username"] = avm.Username;
+                    return View("Welcome");
+                }
+                else {
+                    ViewBag.Error = "Account Invalid";
+                    return View("Index");
+                }
+           }
+    }
+}
---
+++ NewMVCApp/NewMVCAppTest/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewMVCApp.Controllers;
+using NewMVCApp.ViewModels;
+using System.Web.Mvc;
+using NewMVCApp.Models;
+using System.Collections.Concurrent;
+namespace NewMVCAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AccountViewModel avm = new AccountViewModel("abc","1234");
+            //   avm.account.Username = "abc";
+            // avm.account.Password = "1234";
+              // avm.Username = "abc";
+            //   avm.Password = "1234";
+              //AccountController ac = new AccountController();
+              //ViewResult ar = ac.Login(avm) as ViewResult;
+              //Assert.AreEqual("Welcome", ar.ViewName, "loginsuccess");
+              AccountController controller = new AccountController();
+              var result = controller.Login(avm) as ViewResult;
+              Assert.AreEqual("Welcome", result.ViewName);
+              /*var controller = new AccountController();
+              var expected = "Welcome";
+              var actual = ((ViewResult)controller.Index()).ViewName;
+              Assert.AreEqual(expected, actual);*/
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            AccountViewModel avm = new AccountViewModel("abcd","123");
+            AccountController controller = new AccountController();
+            var result = controller.Login(avm) as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+           
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            AccountViewModel avm = new AccountViewModel("abc", "1234");
+            AccountController controller = new AccountController();
+            var result = controller.Login(avm) as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+            
+        }
+    }
+}
